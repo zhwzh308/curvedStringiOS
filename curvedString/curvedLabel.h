52f99c6077819fa851363901ecd1af04bbdd0852
Wenzhong Zhang <zhwzh308@gmail.com>
2014-11-16 01:36:54 -0500

    Add Curved label

---
+++ curvedString/curvedLabel.h
@@ -0,0 +1,13 @@
+//
+//  curvedLabel.h
+//  curvedString
+//
+//  Created by Wenzhong Zhang on 2014-11-16.
+//  Copyright (c) 2014 Wenzhong Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface curvedLabel : UILabel
+
+@end
---
+++ curvedString/curvedLabel.m
@@ -0,0 +1,21 @@
+//
+//  curvedLabel.m
+//  curvedString
+//
+//  Created by Wenzhong Zhang on 2014-11-16.
+//  Copyright (c) 2014 Wenzhong Zhang. All rights reserved.
+//
+
+#import "curvedLabel.h"
+
+@implementation curvedLabel
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
