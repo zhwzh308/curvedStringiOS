933bbd18d65176922a796ae767c93d12b9fe8bb1
Wenzhong Zhang <zhwzh308@gmail.com>
2014-11-16 02:00:39 -0500

    curved label done.

--- curvedString/ViewController.m
+++ curvedString/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import "curvedLabel.h"
 
-@interface ViewController ()
+@interface ViewController () <UITextFieldDelegate>
+@property (weak, nonatomic) IBOutlet curvedLabel *curvedStringLabel;
+@property (weak, nonatomic) IBOutlet UISlider *radiusSlider;
+@property (weak, nonatomic) IBOutlet UITextField *textString;
 
 @end
 
@@ -17,11 +21,31 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    _curvedStringLabel.radius = _radiusSlider.value;
+    _curvedStringLabel.string = _textString.text;
+    _curvedStringLabel.transform = CGAffineTransformMakeScale(1.f, -1.f);
+    //_curvedStringLabel.font = [
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)updateRadius:(UISlider *)sender {
+    _curvedStringLabel.radius = _radiusSlider.value;
+    [_curvedStringLabel setNeedsDisplay];
+}
+
+- (void)textFieldDidEndEditing:(UITextField *)textField {
+    NSLog(@"update text");
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [textField resignFirstResponder];
+    _curvedStringLabel.string = textField.text;
+    [_curvedStringLabel setNeedsDisplay];
+    return NO;
 }
 
 @end
