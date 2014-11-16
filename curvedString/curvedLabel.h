691442e198e01119ea7cbd211d36059d68f3c727
Wenzhong Zhang <zhwzh308@gmail.com>
2014-11-16 01:38:58 -0500

    class methods and draw rect code

--- curvedString/curvedLabel.h
+++ curvedString/curvedLabel.h
@@ -10,4 +10,10 @@
 
 @interface curvedLabel : UILabel
 
+@property (nonatomic) UIFont *font;
+@property (nonatomic) NSString *string;
+@property (readonly, nonatomic) NSAttributedString *attributedString;
+@property (nonatomic) CGFloat radius;
+@property (nonatomic) BOOL showsGlyphBounds, showsLineMetrics, dimsSubstitutedGlyphs;
+
 @end
